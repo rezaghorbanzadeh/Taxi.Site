a59549e9398200added552578f882ca4869f960e
reza <rdaqrbanzadh206@gmail.com>
2025-07-08 22:10:25 +0330

    generate time and randome code

---
+++ Taxi.Core/Generators/CodeGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Taxi.Core.Generators
+{
+    public static class CodeGenerator
+    {
+        //generate code for login and register 6 digit
+        public static string GetActiveCode()
+        {
+            Random random = new Random();
+            return random.Next(100000,990000).ToString();
+        }
+
+        //generate Guid for key 
+        public static Guid GetId() 
+        { 
+            return Guid.NewGuid();
+        }
+
+        //generate name for file(upload file)
+        public static string GetFileName()
+        { 
+            return Guid.NewGuid().ToString().Replace("-","");
+        }
+
+        //generate code for order code
+        public static string GetOrderCode()
+        {
+            Random random = new Random();
+            return random.Next(10000000, 99000000).ToString();
+        }
+    }
+}
---
+++ Taxi.Core/Generators/DataTimeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Globalization;
+
+namespace Taxi.Core.Generators
+{
+    public static class DataTimeGenerator
+    {
+        public static string GetShamsiDate()
+        {
+            // 0000/00/00
+            PersianCalendar pc = new PersianCalendar();
+
+            return pc.GetYear(DateTime.Now).ToString("000") + "/" +
+                   pc.GetMonth(DateTime.Now).ToString("00") + "/" +
+                   pc.GetDayOfMonth(DateTime.Now).ToString("00");
+        }
+        public static string GetShamsiTime()
+        {
+            // 00:00:00
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetHour(DateTime.Now).ToString("00") + ":" +
+              pc.GetMinute(DateTime.Now).ToString("00") + "/" +
+              pc.GetSecond(DateTime.Now).ToString("00");
+        }
+    }
+}
