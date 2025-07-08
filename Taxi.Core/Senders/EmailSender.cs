508d471494dd6312d510630501fc208c5d22543c
reza <rdaqrbanzadh206@gmail.com>
2025-07-08 20:01:32 +0330

    Add project files.

---
+++ Taxi.Core/Senders/EmailSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace Taxi.Core.Senders
+{
+    static class EmailSender
+    {
+
+        public static void SendGmail()
+        {
+            var myMail = "";
+            var mail = new MailMessage();
+        }
+    }
+}
---
+++ Taxi.Core/Senders/SmsSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kavenegar;
+
+namespace Taxi.Core.Senders
+{
+    public static class SmsSender
+    {
+
+        public static void VeryFySend(string toUser, string template, string token)
+        {
+
+            var api = new KavenegarApi("Your api kavengar");
+
+            var StrTo = toUser;
+            var strTamplate = template;
+            var strToken = token;
+
+            api.VerifyLookup(StrTo, strToken, strTamplate);
+        }
+    }
+
+}
